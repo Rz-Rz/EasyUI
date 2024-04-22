c32d8e5034ff5c4d08f81c0bd0ab70856d96419a
Gabriel Moreno <gdmorenor@unal.edu.co>
2024-04-22 14:54:48 +0200

    adding functions to scoreboard to see player name in the screen

--- Scoreboard.js
+++ Scoreboard.js
@@ -1,6 +1,46 @@
+
+ // export default class Scoreboard { 
+//   constructor(parentId) {
+//     this.parent = document.getElementById(parentId);
+//     this.scores = {player1: 0, player2: 0};
+//     this.init();
+//   }
+
+//   init() {
+//     this.scoreboardElement = document.createElement('div');
+//     this.scoreboardElement.id = 'scoreboard';
+//     this.scoreboardElement.innerHTML = `
+//     Player 1: <span id="score1">0</span> | Player 2: <span id="score2">0</span>`;
+//     //CSS Styles
+//     this.scoreboardElement.style.position = 'absolute';
+//     this.scoreboardElement.style.top = '10px';
+//     this.scoreboardElement.style.left = '50%';
+//     this.scoreboardElement.style.transform = 'translateX(-50%)';
+//     this.scoreboardElement.style.color = '#fff';
+//     this.scoreboardElement.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
+//     this.scoreboardElement.style.padding = '10px';
+//     this.scoreboardElement.style.borderRadius = '5px';
+//     this.scoreboardElement.style.fontFamily = 'Arial, sans-serif';
+
+//     this.parent.appendChild(this.scoreboardElement);
+//   }
+
+//   updateScore(player, score) {
+//     if (this.scores[player] !== undefined){
+//     this.scores[player] = score; // Increment score
+//       const playerId = player === 'player1' ? 'score1' : 'score2';
+//     this.scoreboardElement.querySelector(`#${playerId}`).textContent = this.scores[player];
+//     } 
+//     else {
+//       console.error("Invalid player ID: ", player);
+//     }
+//   }
+// }
 export default class Scoreboard { 
   constructor(parentId) {
     this.parent = document.getElementById(parentId);
+    this.player1Name = "player 1";
+    this.player2Name = "player 2";
     this.scores = {player1: 0, player2: 0};
     this.init();
   }
@@ -8,8 +48,7 @@ export default class Scoreboard {
   init() {
     this.scoreboardElement = document.createElement('div');
     this.scoreboardElement.id = 'scoreboard';
-    this.scoreboardElement.innerHTML = `
-    Player 1: <span id="score1">0</span> | Player 2: <span id="score2">0</span>`;
+    this.updateScoreboard();
     //CSS Styles
     this.scoreboardElement.style.position = 'absolute';
     this.scoreboardElement.style.top = '10px';
@@ -26,12 +65,27 @@ export default class Scoreboard {
 
   updateScore(player, score) {
     if (this.scores[player] !== undefined){
-    this.scores[player] = score; // Increment score
+      this.scores[player] = score; // Increment score
       const playerId = player === 'player1' ? 'score1' : 'score2';
-    this.scoreboardElement.querySelector(`#${playerId}`).textContent = this.scores[player];
+      this.scoreboardElement.querySelector(`#${playerId}`).textContent = this.scores[player];
     } 
     else {
       console.error("Invalid player ID: ", player);
     }
+  }
+
+  updatePlayer1Name(player1Name) {
+    this.player1Name = player1Name;
+    this.updateScoreboard();
+  }
+
+  updatePlayer2Name(player2Name) {
+    this.player2Name = player2Name;
+    this.updateScoreboard();
+  }
+
+  updateScoreboard() {
+    this.scoreboardElement.innerHTML = `
+    ${this.player1Name}: <span id="score1">${this.scores.player1}</span> | ${this.player2Name}: <span id="score2">${this.scores.player2}</span>`;
   }
 }
